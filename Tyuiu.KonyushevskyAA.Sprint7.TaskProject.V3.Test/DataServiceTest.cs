1d8d24e8ecf31de6b78759adc114e1f8c9602c34
Tyuiu.KonyushevskyAA@outlook.com <aleksej.konyushevskij@mail.ru>
2023-12-12 21:52:19 +0500

    work

---
+++ Tyuiu.KonyushevskyAA.Sprint7.TaskProject.V3.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KonyushevskyAA.Sprint7.TaskProject.V3.Lib;
+
+namespace Tyuiu.KonyushevskyAA.Sprint7.TaskProject.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
